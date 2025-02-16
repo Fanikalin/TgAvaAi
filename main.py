0336b3046e64c66f15bc76df48b04ceb9c7173db
Fanikalin <bazilbaggins@gmail.com>
2025-02-16 19:30:11 +0300

    -

--- main.py
+++ main.py
@@ -36,19 +36,18 @@ class registration:
 def api_start_button_on_click():
     pass
 
-
 with gr.Blocks(theme=theme) as demo:
-    with gr.Row():
-        with gr.Group() as api_group:
-            api_id = gr.Text(label='api_id')
-            api_hash = gr.Text(label='api_hash')
-            api_start_button = gr.Button(value='api_start_button')
+    #with gr.Row():
+    #    with gr.Group() as api_group:
+    #        api_id = gr.Text(label='api_id')
+    #        api_hash = gr.Text(label='api_hash')
+    #        api_start_button = gr.Button(value='api_start_button')
 
-            api_start_button.click(api_start_button_on_click)
-        with gr.Group() as registration_group:
-            registration.output = gr.Text(value=registration.update_output, label='registration_output', interactive=False, every=2)
-            registration.input = gr.Text(label='registration_input')
+    #        api_start_button.click(api_start_button_on_click)
+    #    with gr.Group() as registration_group:
+    #        registration.output = gr.Text(value=registration.update_output, label='registration_output', interactive=False, every=2)
+    #        registration.input = gr.Text(label='registration_input')
 
-            registration.input.submit(registration.update_input, inputs=[registration.input], outputs=[registration.input])
+    #        registration.input.submit(registration.update_input, inputs=[registration.input], outputs=[registration.input])
 
 demo.launch()
